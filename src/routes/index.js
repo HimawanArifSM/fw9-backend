cca68a986f6db7e10c78fb6cb8c2079fa7e96982
HimawanArifSM <himawanarif7@gmail.com>
2022-06-29 03:19:05 +0700

    fix: bug amount not found in index

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,6 @@ const router = require('express').Router();
 router.use('/users', require('./users'));
 router.use('/transactions', require('./transactions'));
 router.use('/profiles', require('./profiles'));
-//router.use('/transtypes', require('./transtypes'));
+router.use('/transtypes', require('./transtypes'));
 
 module.exports = router;
