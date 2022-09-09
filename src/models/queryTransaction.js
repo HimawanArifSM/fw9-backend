7ae90285107e7ec233dd779929b0313405d635ff
HimawanArifSM <himawanarif7@gmail.com>
2022-09-09 19:32:14 +0700

    fix: transfer

--- src/models/queryTransaction.js
+++ src/models/queryTransaction.js
@@ -7,9 +7,9 @@ exports.transfer=(sender_id, data, cb)=>{
       console.log('error 1');
     }
     else{
-      const time1 = new Date();
+      // const time1 = new Date();
       const q= 'INSERT INTO transactions(notes, recipient_id, sender_id, amount, time, type_id) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *';
-      const val = [data.notes, data.recipient_id, sender_id, parseInt(data.amount), time1, data.type_id];
+      const val = [data.notes, data.recipient_id, sender_id, parseInt(data.amount), data.time, data.type_id];
       db.query(q, val, (err, res)=>{
         if(err){
           console.log('error 2');
